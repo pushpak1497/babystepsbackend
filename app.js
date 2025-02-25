3fcd2ba071cf94a573693741228e2c0322478007
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2025-02-25 14:37:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://babystepsfrontend.vercel.app",
   })
 );
 app.use(express.json({ limit: "30mb" }));
